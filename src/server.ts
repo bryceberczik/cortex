bdbff7a717c42b18da8694c4c2ae009b73cafb54
VanceGoblin <zvkubajak@gmail.com>
2025-07-19 18:14:49 -0500

    Wrote boilerplate for server.ts

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import path from "path";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT!;
+
+app.use(cors());
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(process.cwd(), "../client/dist")));
+// app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on PORT ${PORT}.`);
+});

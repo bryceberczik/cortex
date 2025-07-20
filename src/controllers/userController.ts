105a935edd3e436ef92db25ec1422116fcb9576f
VanceGoblin <zvkubajak@gmail.com>
2025-07-19 21:14:29 -0500

    Created user, post, and comment Prisma models

---
+++ src/controllers/userController.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "../../generated/prisma";
+
+const prisma = new PrismaClient();
+
+export const getAllUsers = async (_req: Request, res: Response) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+  } catch (error) {
+    console.error("Error fetching all users:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const getUserById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const user = await prisma.user.findUnique({ where: { id } });
+    if (!user) {
+      res.status(404).json({ message: "User not found." });
+      return;
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.error("Error fetching user by ID:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};

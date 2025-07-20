4a3636d6ed23a081af734ab0611302fedd5d37c5
bryceberczik <berczikbryce@gmail.com>
2025-07-20 10:31:27 -0500

    postController finished for now

--- src/config/contentfulConfig.ts
+++ src/config/contentfulConfig.ts
@@ -1,8 +1,14 @@
-import { createClient, Entry } from "contentful";
+import { createClient } from "contentful";
 
 const SPACE_ID = process.env.CONTENTFUL_SPACE_ID!;
 const ENVIRONMENT = process.env.CONTENTFUL_ENVIRONMENT!;
 const ACCESS_TOKEN = process.env.CONTENTFUL_ACCESS_TOKEN!;
+
+type Post = {
+  title: string;
+  content: string;
+  postedAt: string;
+};
 
 const contentfulClient = createClient({
   space: SPACE_ID,
@@ -10,9 +16,15 @@ const contentfulClient = createClient({
   accessToken: ACCESS_TOKEN,
 });
 
-export async function fetchPosts(contentType: string): Promise<Entry[]> {
+export async function fetchPosts(contentType: string): Promise<Post[]> {
   const response = await contentfulClient.getEntries({
     content_type: contentType,
+    select: ["fields.title", "fields.content", "fields.postedAt"],
   });
-  return response.items;
+
+  return response.items.map((item) => ({
+    title: item.fields.title as string,
+    content: item.fields.content as string,
+    postedAt: item.fields.postedAt as string,
+  }));
 }
--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -1,13 +1,30 @@
 import { Request, Response } from "express";
 import { fetchPosts } from "../config/contentfulConfig";
+import { PrismaClient } from "../../generated/prisma";
+
+const prisma = new PrismaClient();
 
 export const getAllPosts = async (_req: Request, res: Response) => {
   try {
-    const posts = await fetchPosts("post");
+    const posts = await prisma.post.findMany();
+
+    if (posts.length === 0) {
+      res.status(200).json({ message: "No posts found." });
+      return;
+    }
 
     res.status(200).json(posts);
   } catch (error) {
     console.error("Error fetching all posts:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const createPost = async (req: Request, res: Response) => {
+  try {
+    
+  } catch (error) {
+    console.error("Error creating prisma entry:", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };

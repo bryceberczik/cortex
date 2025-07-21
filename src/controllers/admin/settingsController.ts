3d7a0063a91ed30d238e7cd4726b0bf4a217179e
VanceGoblin <zvkubajak@gmail.com>
2025-07-21 15:02:21 -0500

    Added settings endpoints for removing user and comment & clearing a post's comments

--- src/controllers/admin/settingsController.ts
+++ src/controllers/admin/settingsController.ts
@@ -1,4 +1,53 @@
-// TODO: settings controller
-// - deleteUser
-// - deleteComment
-// - clearPostComments
+import { Request, Response } from "express";
+import { PrismaClient } from "../../../generated/prisma";
+import idSchema from "../../schemas/idSchema";
+
+const prisma = new PrismaClient();
+
+export const removeUser = async (req: Request, res: Response) => {
+  const parsedId = idSchema.safeParse(req.params.id);
+  if (!parsedId.success) {
+    res.status(400).json({ message: "Request Parsing Error" });
+    return;
+  }
+
+  try {
+    await prisma.user.delete({ where: { id: parsedId.data } });
+    res.sendStatus(204);
+  } catch (error) {
+    console.error("Error removing user:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const removeComment = async (req: Request, res: Response) => {
+  const parsedId = idSchema.safeParse(req.params.id);
+  if (!parsedId.success) {
+    res.status(400).json({ message: "Request Parsing Error" });
+    return;
+  }
+
+  try {
+    await prisma.comment.delete({ where: { id: parsedId.data } });
+    res.sendStatus(204);
+  } catch (error) {
+    console.error("Error removing comment:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const clearPostComments = async (req: Request, res: Response) => {
+  const parsedId = idSchema.safeParse(req.params.id);
+  if (!parsedId.success) {
+    res.status(400).json({ message: "Request Parsing Error" });
+    return;
+  }
+
+  try {
+    await prisma.comment.deleteMany({ where: { postId: parsedId.data } });
+    res.sendStatus(204);
+  } catch (error) {
+    console.error("Error clearing post's comments:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
--- src/routes/admin/settingsRoutes.ts
+++ src/routes/admin/settingsRoutes.ts
@@ -1,5 +1,16 @@
 import { Router } from "express";
+import {
+  removeUser,
+  removeComment,
+  clearPostComments,
+} from "../../controllers/admin/settingsController";
 
 const router = Router();
+
+router.delete("/user/:id", removeUser);
+
+router.delete("/comment/:id", removeComment);
+
+router.delete("clear-post/:id", clearPostComments);
 
 export { router as settingsRouter };
